40b3076620a499e06515a674b3e77f14585db5b4
WendaZheng <dylan.d.zheng@gmail.com>
2021-11-06 12:26:04 +0100

    Initial commit

---
+++ Horse-racing Duals/solution.py
@@ -0,0 +1,21 @@
+import sys
+
+#Read the inputs.
+N = int(input())
+
+P = [0] * N
+
+for i in range(N):
+    P[i] = int(input())
+
+#Sort horse strengths.
+P.sort()
+
+#Find D.
+D = sys.maxsize
+
+for i in range(1, N):
+    D = min(D, P[i] - P[i - 1])
+
+#Print D.
+print(D)
